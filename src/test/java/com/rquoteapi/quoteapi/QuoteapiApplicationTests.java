724225fc5bbbc5e3a594daa6906f4d61ca14ede2
FilipposBagordakis <filipposbagordakis@gmail.com>
2022-05-22 07:19:43 +0300

    Fixed the documentation

--- src/test/java/com/rquoteapi/quoteapi/QuoteapiApplicationTests.java
+++ src/test/java/com/rquoteapi/quoteapi/QuoteapiApplicationTests.java
@@ -1,9 +1,20 @@
 package com.rquoteapi.quoteapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(QuoteController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 class QuoteapiApplicationTests {
 
 	@Autowired
@@ -15,9 +26,8 @@ class QuoteapiApplicationTests {
 
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/quote"))
-		.andDo(document("home", responseFields(
-        fieldWithPath("message").description("The welcome message for the user.");
+        this.mockMvc.perform(get("/quote")).andDo(print()).andExpect(status().isOk())
+				.andDo(document("home"));
     }
 
 }
